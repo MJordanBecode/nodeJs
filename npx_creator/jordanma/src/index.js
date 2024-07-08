fd13f6ed376513cc1fc547cc9a0c941c4d0a7495
Masy Jordan <masyjordan@gmail.com>
2024-07-08 12:23:08 +0200

    update

---
+++ npx_creator/jordanma/src/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+//index.js
+// import boxen from 'boxen';
+
+
+let urlGithug = "https://github.com/MJordanBecode";
+let urlLinkedn = "https://www.linkedin.com/in/jordan-masy/";
+
+// console.log(boxen('unicorn', {padding: 1}));
+
+console.log("Welcome on my Card !");
+console.log("My Github : " + urlGithug );
+console.log("My Linkdn : " + urlLinkedn);
